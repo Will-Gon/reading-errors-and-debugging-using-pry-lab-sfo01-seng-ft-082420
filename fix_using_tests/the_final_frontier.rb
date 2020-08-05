3b9e1e9a7dee64547f18b2bd9bbd9d84bead0d9f
Will Gonzalez <will.colocho@gmail.com>
2020-08-05 04:13:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,8 +8,8 @@ end
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-#binding.pry
+binding.pry
 def engage
-  puts state_log(star_date)
-   star_date = generate_star_date
+  puts state_log(date)
+   date = generate_star_date
 end

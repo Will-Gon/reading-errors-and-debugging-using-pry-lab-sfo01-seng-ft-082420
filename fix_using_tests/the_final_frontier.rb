52c21bf6d181631d17b05ee73d23dff7b7d6a833
Will Gonzalez <will.colocho@gmail.com>
2020-08-05 04:19:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,11 +3,11 @@ require 'pry'
 def generate_star_date 
   (rand(100000) + 400000) / 10.0
 end
-def state_log(generate_star_date)
+def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
-binding.pry
+#binding.pry
 def engage
   puts state_log(date)
-   date = generate_star_date
+   date == generate_star_date
 end
